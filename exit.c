eeb58018c1b777c401427942c10269c6733d503e
KCUBE21 <138729734+KCUBE21@users.noreply.github.com>
2024-03-01 23:38:25 +0000

    Update exit.c

--- exit.c
+++ exit.c
@@ -1,30 +1,30 @@
-#include "shell.h"
+#include "main.h"
 
 /**
- * exit_shell - exits the shell
- * @datarel: the data relevant (status and args)
+ * exit_shell - it exits the shell
+ * @datash: the data relevant (status and args)
  * Return: Always returns 0 on success.
  */
-int exit_shell(data_shell *datarel)
+int exit_shell(data_shell *datash)
 {
 	unsigned int ustatus;
 	int is_digit;
 	int str_len;
 	int big_number;
 
-	if (datarel->args[1] != NULL)
+	if (datash->args[1] != NULL)
 	{
-		ustatus = _atoi(datarel->args[1]);
-		is_digit = _isnum(datarel->args[1]);
-		str_len = _strlen(datarel->args[1]);
+		ustatus = _atoi(datash->args[1]);
+		is_digit = _isdigit(datash->args[1]);
+		str_len = _strlen(datash->args[1]);
 		big_number = ustatus > (unsigned int)INT_MAX;
 		if (!is_digit || str_len > 10 || big_number)
 		{
-			call_error(datarel, 2);
-			datarel->status = 2;
+			get_error(datash, 2);
+			datash->status = 2;
 			return (1);
 		}
-		datarel->status = (ustatus % 256);
+		datash->status = (ustatus % 256);
 	}
 	return (0);
 }
