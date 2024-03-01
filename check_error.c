e858c162efd2f7bd84b24eb72cb74ff851856a12
KCUBE21 <138729734+KCUBE21@users.noreply.github.com>
2024-03-01 23:34:38 +0000

    Update check_error.c

--- check_error.c
+++ check_error.c
@@ -1,29 +1,29 @@
-#include "shell.h"
+#include "main.h"
 
 /**
- * char_repeated - counts the repetitions of a char
- * @input: input string
- * @i: index
+ * repeated_char - it counts the repetitions of a char
+ * @input: the input string
+ * @i: the index
  * Return: always  repetitions
  */
 
-int char_repeated(char *input, int i)
+int repeated_char(char *input, int i)
 {
 	if (*(input - 1) == *input)
-		return (char_repeated(input - 1, i + 1));
+		return (repeated_char(input - 1, i + 1));
 
 	return (i);
 }
 
 /**
- * syn_error - finds syntax errors
+ * error_sep_op - it finds syntax errors
  * @input: the input string
  * @i: the index
- * @last: the last char that was read
- * Return: returns index of error or 0 when there are no
+ * @last: the last char read
+ * Return: returns index of error. 0 when there are no
  * errors
  */
-int syn_error(char *input, int i, char last)
+int error_sep_op(char *input, int i, char last)
 {
 	int counts;
 
@@ -32,7 +32,7 @@ int syn_error(char *input, int i, char last)
 		return (0);
 
 	if (*input == ' ' || *input == '\t')
-		return (syn_error(input + 1, i + 1, last));
+		return (error_sep_op(input + 1, i + 1, last));
 
 	if (*input == ';')
 		if (last == '|' || last == '&' || last == ';')
@@ -45,7 +45,7 @@ int syn_error(char *input, int i, char last)
 
 		if (last == '|')
 		{
-			counts = char_repeated(input, 0);
+			counts = repeated_char(input, 0);
 			if (counts == 0 || counts > 1)
 				return (i);
 		}
@@ -58,20 +58,20 @@ int syn_error(char *input, int i, char last)
 
 		if (last == '&')
 		{
-			counts = char_repeated(input, 0);
+			counts = repeated_char(input, 0);
 			if (counts == 0 || counts > 1)
 				return (i);
 		}
 	}
 
-	return (syn_error(input + 1, i + 1, *input));
+	return (error_sep_op(input + 1, i + 1, *input));
 }
 
 /**
- * first_char - finds index of the first char
+ * first_char - the finds index of the first char
  * @input: the input string
  * @i: the index
- * Return: Always return 1 if there is an error, 0 in other cases.
+ * Return: Always return 1 if there is an error. 0 in other case.
  */
 
 int first_char(char *input, int *i)
@@ -92,15 +92,15 @@ int first_char(char *input, int *i)
 }
 
 /**
- * print_syn_error - prints when a syntax error is found
- * @datarel: the data structure
+ * print_syntax_error - it prints when a syntax error is found
+ * @datash: the data structure
  * @input: the input string
  * @i: the index of the error
  * @bool: it control msg error
- * Return: returns nothing
+ * Return: it  returns nothing
  */
 
-void print_syn_error(data_shell *datarel, char *input, int i, int bool)
+void print_syntax_error(data_shell *datash, char *input, int i, int bool)
 {
 	char *msg, *msg2, *msg3, *error, *counter;
 	int length;
@@ -121,8 +121,8 @@ void print_syn_error(data_shell *datarel, char *input, int i, int bool)
 
 	msg2 = ": Syntax error: \"";
 	msg3 = "\" unexpected\n";
-	counter = hsh_itoa(datarel->counter);
-	length = _strlen(datarel->av[0]) + _strlen(counter);
+	counter = hsh_itoa(datash->counter);
+	length = _strlen(datash->av[0]) + _strlen(counter);
 	length += _strlen(msg) + _strlen(msg2) + _strlen(msg3) + 2;
 
 	error = malloc(sizeof(char) * (length + 1));
@@ -131,7 +131,7 @@ void print_syn_error(data_shell *datarel, char *input, int i, int bool)
 		free(counter);
 		return;
 	}
-	_strcpy(error, datarel->av[0]);
+	_strcpy(error, datash->av[0]);
 	_strcat(error, ": ");
 	_strcat(error, counter);
 	_strcat(error, msg2);
@@ -145,13 +145,13 @@ void print_syn_error(data_shell *datarel, char *input, int i, int bool)
 }
 
 /**
- * check_syn_error - intermediate function to
+ * check_syntax_error - it intermediate function to
  * find and print a syntax error
- * @datarel: the data structure
+ * @datash: the data structure
  * @input: the input string
  * Return: Always return 1 if there is an error. 0 in other case
  */
-int check_syn_error(data_shell *datarel, char *input)
+int check_syntax_error(data_shell *datash, char *input)
 {
 	int begin = 0;
 	int f_char = 0;
@@ -160,14 +160,14 @@ int check_syn_error(data_shell *datarel, char *input)
 	f_char = first_char(input, &begin);
 	if (f_char == -1)
 	{
-		print_syn_error(datarel, input, begin, 0);
+		print_syntax_error(datash, input, begin, 0);
 		return (1);
 	}
 
-	i = syn_error(input + begin, 0, *(input + begin));
+	i = error_sep_op(input + begin, 0, *(input + begin));
 	if (i != 0)
 	{
-		print_syn_error(datarel, input, begin + i, 1);
+		print_syntax_error(datash, input, begin + i, 1);
 		return (1);
 	}
 
